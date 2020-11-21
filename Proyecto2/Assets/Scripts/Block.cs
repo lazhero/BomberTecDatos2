7316c5f057041040375f38f71f0303d852f8deca
Adrian <adriantec2019@gmail.com>
2020-11-21 16:01:37 -0600

    destructible and not destructible blocks prefab

---
+++ Proyecto2/Assets/Scripts/Block.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField]
+    private bool isDestructible=false;
+    [SerializeField] private GameObject dead_Object;
+    
+    private bool isDestroy=false;
+    
+    public bool dest=false;
+
+
+
+    private void Update() {
+        if(dest)
+            DestroyMe();
+    }
+    
+    void Destr() {
+        Destroy(gameObject);
+    }
+
+    private int force = 12;
+    private void DestroyMe()
+    {
+        if (!isDestructible || isDestroy) return;
+        isDestroy = true;
+        var producto= Instantiate(dead_Object);
+        producto.transform.position = transform.position;
+                    
+        Invoke("Destr",0.1f);
+
+    }
+}
---
+++ Proyecto2/Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    [SerializeField]
+    private float Time= 1.5f;
+    void Start()
+    {
+        Invoke("Destr",Time);
+    }
+
+    
+    void Destr()
+    {
+        Destroy(gameObject);
+    }
+}
