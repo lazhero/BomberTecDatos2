798232d651fd2c87e7a0076f7c43833c6f580ac4
lazhero <luisandreyzuniga@gmail.com>
2020-11-20 18:41:15 -0600

    Deleting the explosion before it ends

---
+++ Proyecto2/Assets/Scripts/DeleteExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private ParticleSystem particleSystem;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!particleSystem.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
