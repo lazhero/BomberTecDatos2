a292b99dbb3aa8c8169dedd58e9f0f308c02a1fb
miguemesen <miguemesen12@gmail.com>
2020-11-29 05:10:51 -0600

    Power ups can be consumed

--- Proyecto2/Assets/Scripts/Consumable.cs
+++ Proyecto2/Assets/Scripts/Consumable.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Consumable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int healthRestoration = 1;
+
+    private Collider _collider;
+
+    private void Awake()
     {
-        
+        _collider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Adding Health");
+            other.SendMessageUpwards("AddHealth", healthRestoration);
+            _collider.enabled = false;
+            Destroy(gameObject);
+        }
     }
 }
