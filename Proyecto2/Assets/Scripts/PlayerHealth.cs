4cd4a5c0c94657b7e7efc722aef87c38bebfe7ad
miguemesen <miguemesen12@gmail.com>
2020-12-01 01:52:14 -0600

    Safe commit

--- Proyecto2/Assets/Scripts/PlayerHealth.cs
+++ Proyecto2/Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    
+    public int totalHealth = 3;
+    private int health;
+    private float heartSize = 16f;
+
+    public RectTransform heartUI;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = totalHealth;   
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddDamage(int amount)
     {
+        health = health - amount;
         
+        // Game  Over
+        if (health <= 0) {
+            health = 0;
+        }
+
+        heartUI.sizeDelta = new Vector2(heartSize * health, 14);
+        Debug.Log("Player got damaged. His current health is " + health);
+    }
+
+    public void AddHealth(int amount)
+    {
+        health = health + amount;
+
+        // Max health
+        if (health > totalHealth) {
+            health = totalHealth;
+        }
+        
+        heartUI.sizeDelta = new Vector2(heartSize * health, 14);
+        Debug.Log("Player got some life. His current health is " + health);
     }
 }
