3dd92481f334b217a92edb0bbed13ed11574f8f3
miguemesen <miguemesen12@gmail.com>
2020-12-04 03:23:23 -0600

    Added stats to players

--- Proyecto2/Assets/Scripts/Stats.cs
+++ Proyecto2/Assets/Scripts/Stats.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+
+    public int speed;
+    public int life;
+    public int dodge;
+    public int expRatio;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        setStats();
     }
 
-    // Update is called once per frame
-    void Update()
+    void setStats()
     {
-        
+        speed = Random.Range(4, 7);
+        life = Random.Range(3, 6);
+        expRatio = Random.Range(3, 6);
+        dodge = 20 - speed - life - expRatio; // 20 is the sum of all the stats
     }
+    
 }
