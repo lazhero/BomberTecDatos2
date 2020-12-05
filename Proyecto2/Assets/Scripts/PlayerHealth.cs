0b133962be64dea17663781f4404085df026cfc1
miguemesen <miguemesen12@gmail.com>
2020-12-05 02:39:36 -0600

    Safe commit

--- Proyecto2/Assets/Scripts/PlayerHealth.cs
+++ Proyecto2/Assets/Scripts/PlayerHealth.cs
@@ -9,76 +9,6 @@ public class PlayerHealth : MonoBehaviour
 {
 
     private bool canReciveDamage=true;
-    
-    public int totalBomb = 1;
-    private int _bomb;
-    
-
-    private int Bomb
-    {
-        get => _bomb;
-        set
-        {
-            Bomb =  value;
-            if (Bomb > totalBomb)
-            {
-                Bomb = totalBomb;
-                bombUi.sizeDelta = new Vector2(_bombSize * Bomb, 14.48443f);
-            }
-
-            if (Bomb <= 0)
-            {
-                Bomb = 0;
-                bombUi.sizeDelta = new Vector2(Bomb * Bomb, 14.48443f);
-            }
-
-
-        }
-    }
-
-    public int totalShoe = 3;
-    private int _shoe;
-
-    private int Shoe
-    {
-
-        get => _shoe;
-        set
-        {
-            Shoe = value;
-
-            if (Shoe > totalShoe)
-                Shoe = totalShoe;
-            if (Shoe <= 0)
-                Shoe = 0;
-            shoeUi.sizeDelta = new Vector2(_shoeSize * Shoe, 13.00002f);
-
-        }
-
-    }
-
-    public int totalShield = 3;
-    private int _shield;
-
-    private int Shield
-    {
-        set
-        {
-            Shield = value;
-
-            if (Shield > totalShield)
-                Shield = totalShield;
-
-            if (Shield <= 0)
-                Shield = 0;
-
-            shieldUi.sizeDelta = new Vector2(_shieldSize * Shield, 15.9688f);
-
-        }
-
-        get => _shield;
-    }
-
 
     public int totalHealth;
     public int health;
@@ -109,17 +39,24 @@ public class PlayerHealth : MonoBehaviour
     }
 
     private float _heartSize = 16f;
-    private float _shoeSize = 13.73899f;
+    private float _shoeSize = 14.6f;
     private float _shieldSize = 12.25836f;
     private float _bombSize = 12.25832f;
+    private float _expSize = 10f;
 
     public RectTransform heartUi;
     public RectTransform shieldUi;
     public RectTransform shoeUi;
     public RectTransform bombUi;
 
+    public int totalShoe;
+    public int Shoe;
+    
     void Start()
     {
+        totalShoe = gameObject.GetComponent<Stats>().speed;
+        Shoe = totalShoe;
+        shoeUi.sizeDelta = new Vector2(_shoeSize * Shoe, 13.00002f);
         totalHealth = gameObject.GetComponent<Stats>().life;
         Health = totalHealth;
     }
